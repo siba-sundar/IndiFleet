edbf434166fee63f6a894af4828f1ecedf30c21c
Siba Sundar <siba.edu2003@gmail.com>
2025-02-20 13:11:30 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import viteCommonjs from 'vite-plugin-commonjs';  // Import the plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCommonjs()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -13,16 +14,12 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          // Create separate chunks for dependencies
           if (id.includes('node_modules')) {
             return 'vendor';
-          }
-          if (id.includes('src/components')) {
-            return 'components';
           }
         },
       },
     },
-    chunkSizeWarningLimit: 1500,  // Adjust this value based on your needs to suppress warnings
+    chunkSizeWarningLimit: 1500,
   },
 });
